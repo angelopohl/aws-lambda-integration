02f333edc6b86e81d0e832be9297e3c865973660
angelopohl <angelfrancasanovach@gmail.com>
2026-05-06 09:34:26 -0500

    feat: DEfinicion de variables region, project_name y environment

---
+++ iac/variables.tf
@@ -0,0 +1,16 @@
+variable "region" {
+    type = string
+    description = "Region de AWS"
+    default = "us-east-1"
+}
+
+variable "project_name" {
+    type = string
+    description = "Nombre del proyecto"
+    default = "aws-lambda-integration"
+}
+
+variable "environment" {
+    type = string
+    description = "Ambientes del proyecto (DEV, QA y PROD)"
+}
