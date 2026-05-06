482cd46c89b07ead7a2696166d8291a66223ea2f
angelopohl <angelfrancasanovach@gmail.com>
2026-05-06 18:44:41 -0500

    feat(infra): configurar vpc endpoints para acceso privado a s3 y sqs

---
+++ iac/endpoints.tf
@@ -0,0 +1,30 @@
+resource "aws_vpc_endpoint" "s3" {
+  vpc_id            = aws_vpc.main.id
+  service_name      = "com.amazonaws.${var.region}.s3"
+  vpc_endpoint_type = "Gateway"
+
+  route_table_ids = [
+    aws_route_table.private_a.id,
+    aws_route_table.private_b.id
+  ]
+
+  tags = { Name = "${var.project_name}-${var.environment}-vpce-s3" }
+}
+
+resource "aws_vpc_endpoint" "sqs" {
+  vpc_id              = aws_vpc.main.id
+  service_name        = "com.amazonaws.${var.region}.sqs"
+  vpc_endpoint_type   = "Interface"
+  private_dns_enabled = true 
+
+  subnet_ids = [
+    aws_subnet.private_a.id,
+    aws_subnet.private_b.id
+  ]
+
+  security_group_ids = [
+    aws_security_group.vpce_sqs.id
+  ]
+
+  tags = { Name = "${var.project_name}-${var.environment}-vpce-sqs" }
+}
