b1290480e02ba9196cca3b3bee5421992f16b094
angelopohl <angelfrancasanovach@gmail.com>
2026-05-06 09:22:43 -0500

    feat: Confirguracion de proveedor aws y tags generales

---
+++ iac/providers.tf
@@ -0,0 +1,20 @@
+terraform{
+    required_providers{
+        aws = {
+            source = "hashicorp/aws"
+            version = "~> 5.0"
+        }
+    }
+}
+
+provider "aws" {
+    region = var.region
+
+    default-tags{
+        tags={
+         Project = var.project_name
+         ManagedBy = "Terraform"
+         Environment = var.environment   
+        }
+    }
+}
