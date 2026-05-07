550eb962e69ec5737e9321058dcdd4cebf219ee8
angelopohl <angelfrancasanovach@gmail.com>
2026-05-06 19:00:12 -0500

    feat(infra): crear api gateway como punto de entrada publico

--- iac/api.tf
+++ iac/api.tf
@@ -1,1 +1,34 @@
-# api.tf
+resource "aws_apigatewayv2_api" "http_api" {
+  name          = "${var.project_name}-${var.environment}-api"
+  protocol_type = "HTTP"
+  description   = "API Gateway para recibir las imágenes de los usuarios"
+}
+
+resource "aws_apigatewayv2_integration" "upload_integration" {
+  api_id           = aws_apigatewayv2_api.http_api.id
+  integration_type = "AWS_PROXY"
+  
+  integration_uri  = aws_lambda_function.upload_lambda.invoke_arn 
+}
+
+resource "aws_apigatewayv2_route" "upload_route" {
+  api_id    = aws_apigatewayv2_api.http_api.id
+  route_key = "POST /upload"
+  target    = "integrations/${aws_apigatewayv2_integration.upload_integration.id}"
+}
+
+# 4. Creamos un "Stage" (Entorno) que se despliegue automáticamente
+resource "aws_apigatewayv2_stage" "default_stage" {
+  api_id      = aws_apigatewayv2_api.http_api.id
+  name        = "$default"
+  auto_deploy = true
+}
+
+resource "aws_lambda_permission" "api_gw_invoke" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.upload_lambda.function_name
+  principal     = "apigateway.amazonaws.com"
+
+  source_arn = "${aws_apigatewayv2_api.http_api.execution_arn}/*/*"
+}
